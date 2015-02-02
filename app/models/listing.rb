a8bffd96afe6e65296d025d875937ae990e63cfa
shelton <sgbythelake@gmail.com>
2015-02-01 21:10:33 -0800

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,15 +1,20 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
-		has_attached_file :image, :styles => { :medium => "200>", :thumb => "100x100>" }, :default_url => "notfound.jpg"
-	else
-		has_attached_file :image, :styles => { :medium => "200>", :thumb => "100x100>" }, :default_url => "notfound.jpg",
-						  :storage => :dropbox,
-						  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-						  :path => ":style/:id_:filename"
-  		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  	end	
-
-  	validates :name, :description, :price, presence: true
-  	validates :price, numericality: {greater_than: 0}
-  	validates_attachment_presence :image
+if Rails.env.production?
+has_attached_file :image,
+:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+:storage => :dropbox,
+:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+:path => ":style/:id_:filename"
+validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+belongs_to :user
+else
+has_attached_file :image,
+:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+# Previous code from earlier lecture (25 or 26). Lines 13 & 19-24 added for lecture 27.
+validates :name, :description, :price, presence: true
+validates :price, numericality: { greater_than: 0 }
+validates_attachment_presence :image
+validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+belongs_to :user
 end
+end 
